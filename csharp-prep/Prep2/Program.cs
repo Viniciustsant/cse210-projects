9b1d438a9cf634e8972257a1ea6093a1f8f12538
Vinicius Teixeira Santiago <viniciustsantiagosud@gmail.com>
2024-09-19 16:45:04 -0300

    finished Prep 2 with core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        
+        string letter;
+
+        if (gradePercentage < 60)
+        {
+            //Console.WriteLine("Your Grade Percentage is F");
+            letter = "F";
+        }
+
+        else if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            //Console.WriteLine("Your Grade Percentage is D");
+            letter = "D";
+        }
+
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            //Console.WriteLine("Your Grade Percentage is C"); 
+            letter = "C";  
+        }
+
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            //Console.WriteLine("Your Grade Percentage is B");
+            letter = "B";
+        }
+
+        else 
+        {
+            //Console.WriteLine("Your Grade Percentage is A");
+            letter = "A";
+        }
+
+        
+        Console.WriteLine($"Your Grade Percentage is {letter}");
+        
+        //loop to see if passed or not
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed !");
+        }
+
+        else
+        {
+            Console.WriteLine("Unfortunately you haven't passed, but keep going and study more, you can do it.");
+        }
     }
 }

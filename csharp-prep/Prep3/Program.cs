a0c79481a630762309da7b38cba2afe40d34344b
Vinicius Teixeira Santiago <viniciustsantiagosud@gmail.com>
2024-09-26 14:45:20 -0300

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string number = Console.ReadLine();
+        //int magicNumber = int.Parse(number);
+        Random number = new Random();
+        int magicNumber = number.Next(1,100);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your Guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else 
+            {
+                Console.WriteLine("You Guessed it!");
+            }
+        } 
     }
 }

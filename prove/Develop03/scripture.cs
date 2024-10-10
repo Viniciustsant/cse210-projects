c72cda90557900f3c685596900b29709610d0365
Vinicius Teixeira Santiago <viniciustsantiagosud@gmail.com>
2024-10-10 20:47:48 -0300

    first move

---
+++ prove/Develop03/scripture.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Scripture
+{
+    private string scripture = "Cremos na coligação literal de Israel e na restauração das Dez Tribos; que Sião (a Nova Jerusalém) será construída no continente americano; que Cristo reinará pessoalmente na Terra; e que a Terra será renovada e receberá a sua glória paradisíaca.";
+
+
+}
